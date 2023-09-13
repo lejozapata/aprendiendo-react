371299d737feaf0e2febe4a92f79e487dfee770c
lejozapata <lejozapata@gmail.com>
2023-09-13 01:00:28 -0500

    Cambios TwitterFolloWCard

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {App} from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// creo la constante root para mostrar lo que necesito
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// Creo un componente para crear un boton y que en el render pueda elegir los atributos de manera mas comoda
+
+
+//Root render se usa para exponer lo que quiero mostrar
+root.render(
+<React.Fragment>
+    <App/>
+</React.Fragment>
 )
